87343f3b9a95def9b294f11e905c7a365fc49e80
Ubuntu <ubuntu@ip-172-31-2-219.us-west-2.compute.internal>
2018-11-21 02:29:10 +0000

    misplaced directive

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
-
-mount Sidekiq::Web => '/sidekiq'
